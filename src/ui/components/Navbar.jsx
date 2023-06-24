93495906c791e6475a2401ffb115011b84407aaa
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-06-23 23:48:31 -0400

    design homepage

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { NavLink } from 'react-router-dom'
+import Typography from '@mui/material/Typography'
 import '../../styles/navbar.css'
+import logo from '../../logo2.mp4'
 
 const Navbar = () => {
+
+    const video = useRef(null);
+
+    useEffect(() => {
+        video.current.play();
+    }, []);
+
     return (
       <div id="navbar">
-          <NavLink to="/">Home</NavLink>
+        <div className="logo">
+            <video width="100" height="100" ref={video} autoPlay muted>
+                <source src={logo} type='video/mp4'/>
+            </video>
+        </div>
+          <NavLink to="/">
+          <div style={{display: 'flex', alignItems: 'center'}}>
+          <Typography sx={{transform: 'translateY(-10%)', fontFamily: `'Caveat', cursive;`, fontSize: '2.3rem', marginRight: '10px'}}>Zip City</Typography><Typography sx={{fontFamily: `'Mallanna', sans-serif`, fontSize: '1.5rem'}}>Search</Typography>
+          </div>
+          </NavLink>
       </div>
     )
         
--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect, useRef} from 'react'
 import { com } from "../../ui"
-import '../../styles/fonts.css'
+import '../../styles/logo.css'
 
 export default function Home() {
+
     return (
         <div>
+        <div id="logo-animate">
             <div className="animation">
             <com.AnimatedHeader text="Find exactly where you're looking for!"/>
             </div>
+            </div>
             <com.SearchForm/>
-        </div>)
+        </div>
+    )
         
 }
