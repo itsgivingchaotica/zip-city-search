f5c296a5e344aa22d7d7f69e2697c7f24948bdd3
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-06-24 20:09:01 -0400

    create result card

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useRef} from 'react'
-import { com } from "../../ui"f
+import React, { useEffect, useRef, useContext } from 'react'
+import { SearchContext } from '../../SearchContext.js'
+import { com } from "../../ui"
 import '../../styles/logo.css'
 
 export default function Home() {
 
+    const { searchType, setSearchType, searchTerm, setSearchTerm } = useContext(SearchContext)
+
+    const handleSearchEngine = () => {
+
+    }
+
     return (
-        <div>
+        <div className="homepage">
         <div id="logo-animate">
             <div className="animation">
             <com.AnimatedHeader text="Find exactly where you're looking for!"/>
             </div>
         </div>
-        <div style={{transform:'translateY(50%)',display:'flex'}}>
+        <div style={{transform:'translateY(280%)',display:'flex', marginLeft: '50px'}}>
         <com.SearchForm handleSearchEngine={handleSearchEngine}/>
         </div>
         </div>
--- src/ui/pages/Results.jsx
+++ src/ui/pages/Results.jsx
@@ -1,11 +1,54 @@
 import React from 'react'
+import Grid from '@mui/material/Grid'
 import { com } from "../../ui"
+import '../../styles/results.css'
 
 export default function Results() {
     return (
-        <div>
-            <com.Banner/>
-            <com.ResultCard/>
-        </div>)
+        <div className="results">
+        <div id="root">
+        <com.Banner/>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 10, md: 15 }} sx={{justifyContent: "center", paddingTop: '40px' }}>
+          {/* {contacts.map((contact, index) => ( */}
+          {/* <Grid item xs={6} key={index}> */}
+          <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+            <Grid item xs={5}>
+              <com.ResultCard/> 
+            </Grid>
+          {/* ))} */}
+    </Grid>
+            
+        </div>
+        </div>
+    )
         
 }
